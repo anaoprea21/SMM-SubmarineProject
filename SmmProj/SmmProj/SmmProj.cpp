eb3c0e5193d59ebb3b0ad9e92955396bfb2886e7
anaoprea21 <ana.oprea1409@gmail.com>
2022-04-19 00:02:32 +0300

    submarine first attempt
    Drew two polygons for the body and propeller, added animation and keyboard functions for movement

--- SmmProj/SmmProj/SmmProj.cpp
+++ SmmProj/SmmProj/SmmProj.cpp
@@ -1,20 +1,158 @@
-// SmmProj.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include<GL/freeglut.h>
+#include <stdio.h>
 
-#include <iostream>
 
-int main()
+float xt = 1.0, yt = 1.0;            //for keyboard
+float x = 1.0, y = 1.0, z = 1.0;    // for movement
+float angle = 0;  				// for animation
+float Autorun = 300;			// For Movement Autorun
+
+void animation(void)
 {
-    std::cout << "Hello World!\n";
+	if (angle >= 0 && angle < 10)
+		angle = angle + 0.5;
+	else angle = 0;
+	glutPostRedisplay();
 }
+void Auto(void)
+{
+	if (Autorun <= 300 && Autorun > -350)
+		Autorun = Autorun - 0.05;
+	else Autorun = 300;
+	glutPostRedisplay();
+}
+void settings(void)
+{
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glPointSize(5.0);
+	glLineWidth(1.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0, 400.0, 0.0, 400.0);
+}
+void Display(void)
+{
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+	//background color
+	GLclampf Red = 0.0f, Green = 0.0f, Blue = 1.0f, Alpha = 0.0f;
+	glClearColor(Red, Green, Blue, Alpha);
+	glClear(GL_COLOR_BUFFER_BIT);
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	//the submarine
+	glPushMatrix();
+	glPushMatrix();
+	glRotatef(angle, 0.0, 0.0, 0.0);
+	glTranslatef(Autorun, 0.0, 0.0);
+	glTranslatef(xt, yt, 0.0);			//for move 
+
+	//body
+	glBegin(GL_POLYGON);
+	glColor3f(0.0, 1.0, 0.0);
+	glVertex2i(20, 200);//most right
+	glVertex2i(10, 280);//most upper right
+	glVertex2i(100, 300);//middle up
+	glVertex2i(320, 250); //most left up
+	glVertex2i(320, 200); //most left down
+	glVertex2i(100, 160);//middle down
+	glEnd();
+
+	glPushMatrix();
+	glRotatef(angle, 0.0, 0.0, 0.0);
+
+
+	//propeller
+	glBegin(GL_POLYGON);
+	glColor3f(0.0, 1.0, 0.0);
+	glVertex2i(320, 225);
+	glVertex2i(360, 265);
+	glVertex2i(340, 225);
+	glVertex2i(360, 185);
+	glVertex2i(320, 225);
+	glEnd();
+
+	glPopMatrix();
+	glPopMatrix();
+	glutSwapBuffers();
+	glFlush();
+}
+void keyboard(GLubyte key, int x, int y)
+{
+	switch (key)
+	{
+	case 'd':
+		xt += 2.0;
+		glColor3f(0.0, 1.0, 0.0);
+		glutPostRedisplay();
+		break;
+
+	case 'a':
+		xt -= 2.0;
+		glColor3f(1.0, 0.0, 0.0);
+		glutPostRedisplay();
+		break;
+
+	case 's':
+		yt -= 2.0;
+		glColor3f(0.0, 0.0, 0.0);
+		glutPostRedisplay();
+		break;
+
+	case 'w':
+		yt += 2.0;
+		glColor3f(0.0, 0.0, 1.0);
+		glutPostRedisplay();
+		break;
+
+	case 'e':
+		xt += 2.0;
+		yt += 2.0;
+		glColor3f(1.0, 0.0, 1.0);
+		glutPostRedisplay();
+		break;
+
+	case 'q':
+		xt -= 2.0;
+		yt += 2.0;
+		glColor3f(0.0, 1.0, 1.0);
+		glutPostRedisplay();
+		break;
+	case 'c':
+		xt += 2.0;
+		yt -= 2.0;
+		glColor3f(1.0, 0.0, 1.0);
+		glutPostRedisplay();
+		break;
+	case 'z':
+		xt -= 2.0;
+		yt -= 2.0;
+		glColor3f(0.0, 0.0, 1.0);
+		glutPostRedisplay();
+		break;
+
+	default:
+		break;
+	}
+}
+void main(int arg1, char** arg2)
+{
+	//initialize
+	glutInit(&arg1, arg2);
+
+	//single for single buffer, single window
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+
+	//window settings
+	glutInitWindowPosition(200, 50);   //200 Lelt 50 Height
+	glutInitWindowSize(600, 600);
+	glutCreateWindow("Submarine --- first try");//window title
+
+	settings();
+	glutDisplayFunc(Display);
+
+	//animation
+	glutIdleFunc(animation);
+	glutIdleFunc(Auto);
+
+	glutKeyboardFunc(keyboard);
+	glutMainLoop();
+}
